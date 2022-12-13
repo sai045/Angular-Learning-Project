f3a90fef3672d1e45a01167b6acfc05884d9a6cc
sai varshith <saivarshith3041@gmail.com>
2022-12-13 12:00:30 +0530

    Added Initial COmponents and completed Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IngredientComponent } from './ingredient/ingredient.component';
+import { FormsModule } from '@angular/forms';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, IngredientComponent, RecipeComponent, ShoppingComponent, RecipeBookComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule,FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/ingredient/ingredient.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredient',
+  templateUrl: './ingredient.component.html',
+  styleUrls: ['./ingredient.component.css'],
+})
+export class IngredientComponent {
+  ingredientName = '';
+  ingredientPrice = '';
+  ingredientType = '';
+}
