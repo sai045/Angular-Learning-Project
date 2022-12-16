b5f08bca2c79f8e8d9996c78a040b383a1f9cfa0
sai varshith <saivarshith3041@gmail.com>
2022-12-16 15:26:37 +0530

    Beginner Project Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,33 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { IngredientComponent } from './ingredient/ingredient.component';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RecipeComponent } from './recipe/recipe.component';
-import { ShoppingComponent } from './shopping/shopping.component';
-import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+
+
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
-  declarations: [AppComponent, IngredientComponent, RecipeComponent, ShoppingComponent, RecipeBookComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule,FormsModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
