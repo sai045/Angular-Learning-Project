2580f46ae9cf17091be66a8230e25bd19a2b3646
sai varshith <saivarshith3041@gmail.com>
2022-12-22 09:25:20 +0530

    course-project phase-2

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -3,14 +3,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
+  let component: HeaderComponent; 
   let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [HeaderComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
